80ab5fbeec584b01ae5090f2d85853993bde402b
DuoLabz <116619521+DuoLabz@users.noreply.github.com>
2022-11-29 17:00:56 +0500

    Add files via upload

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -7,11 +7,11 @@ import Frame from "../assets/frame.png";
 const Cards = () =>{
 
     return(
-            <Card style={{ width: '15rem',background:"transparent",border:'none' }}>
+            <Card style={{ width: '15rem',background:"transparent",border:'none',color:"#069f90", fontFamily: "myFont"}}>
       <Card.Img variant="top" src={Frame} />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
+        <Card.Title style={{  textShadow: "2px -1px 1px #006359"}}>Card Title</Card.Title>
+        <Card.Text style={{fontSize:"12px"}}>
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </Card.Text>        
